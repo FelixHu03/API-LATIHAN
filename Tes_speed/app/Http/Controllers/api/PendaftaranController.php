7cbc4ab41e6ffcbfc14e76a4e3d58dae4c2a1588
Felix <felix_2327250059@mhs.mdp.ac.id>
2024-10-31 15:26:56 +0700

    update : controller salah pada penaman tabel

--- Tes_speed/app/Http/Controllers/api/PendaftaranController.php
+++ Tes_speed/app/Http/Controllers/api/PendaftaranController.php
@@ -10,96 +10,91 @@ use Illuminate\Support\Facades\Validator;
 
 class PendaftaranController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $pendaftaran = pendaftaran::get();
+        $pendaftaran = Pendaftaran::get();
         if ($pendaftaran) {
             return PendaftaranResource::collection($pendaftaran);
         } else {
             return response()->json(['message'=> 'Tidak Ada Isi'], 200);
         }
-
     }
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'nama' => 'required|string|max:255',
             'email' => 'required|string|max:255',
-            'nomorTelepon' => 'required|string|max:255|unique:pendaftaras,nomorTelepon',
+            'nomorTelepon' => 'required|string|max:255|unique:pendaftarans,nomorTelepon',
             'tingkatSekolah' => 'required|string|max:255',
         ]);
+        
         if ($validator->fails()) {
             return response()->json([
                 'message'=> 'All fields are mandatory',
                 'error' => $validator->messages(),
             ], 422);
-
         } 
-        $pendaftaran = pendaftaran::create([
+
+        $pendaftaran = Pendaftaran::create([
             'nama'=> $request->nama,
             'email'=> $request->email,
-            'nomorTelepon'=> $request->noorTelepon,
+            'nomorTelepon'=> $request->nomorTelepon,
             'tingkatSekolah'=> $request->tingkatSekolah,
-
         ]);
+
         return response()->json([
             'message'=> 'Pendaftaran Berhasil',
             'data'=> new PendaftaranResource($pendaftaran),
-        ],200);
-        
+        ], 200);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show($id)
     {
-        $pendaftaran = pendaftaran::findOrFail($id);
+        $pendaftaran = Pendaftaran::findOrFail($id);
         return new PendaftaranResource($pendaftaran);
     }
+
     public function update(Request $request, $id)
     {
-        $pendaftaran = pendaftaran::findOrFail($id);
+        $pendaftaran = Pendaftaran::findOrFail($id);
+
         $validator = Validator::make($request->all(), [
             'nama' => 'required|string|max:255',
             'email' => 'required|string|max:255',
-            'nomorTelepon' => 'required|string|max:255|unique:pendaftaras,nomorTelepon',
+            'nomorTelepon' => 'required|string|max:255|unique:pendaftarans,nomorTelepon,' . $id,
             'tingkatSekolah' => 'required|string|max:255',
         ]);
+
         if($validator->fails()){
             return response()->json([
-                'message' => 'All fields are mandotory',
+                'message' => 'All fields are mandatory',
                 'error'=> $validator->messages(),
-            ],422);
+            ], 422);
         }   
+
         $pendaftaran->update([
             'nama'=> $request->nama,
             'email'=> $request->email,
             'nomorTelepon'=> $request->nomorTelepon,
             'tingkatSekolah'=> $request->tingkatSekolah,
         ]);
+
         $pendaftaran->refresh();
+
         return response()->json([
             'message'=> 'Data Berhasil di Update',
             'data' => new PendaftaranResource($pendaftaran),
-        ],200);
+        ], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy($id)
     {
         $pendaftaran = Pendaftaran::findOrFail($id);
         $pendaftaran->delete();
+
         return response()->json([
-            'message' => "Peserta Berhasil di hapus",
-        ],200);
+            'message' => "Peserta Berhasil dihapus",
+        ], 200);
     }
 }
